c28bf128ff353813071f977a0a8f301b94d26a76
caoych5 <caoych5@uchicago.edu>
2021-04-02 12:10:12 +0800

    class 1 example

---
+++ class1testing.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 31 12:35:20 2021
+
+@author: caoyc
+"""
+
+# Demo file for Spyder Tutorial
+# Hans Fangohr, University of Southampton, UK
+
+def hello():
+    """Print "Hello World" and return None."""
+    print("Goodbye World")
+
+# Main program starts here
+hello()
+x = 10
+print(x*2)
